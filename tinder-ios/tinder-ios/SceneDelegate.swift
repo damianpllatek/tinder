9b894ccb27e16a8db497ac82ab3f6644be8b4bf4
Damian Płatek <damian.platek@infakt.pl>
2024-10-15 15:02:13 +0200

    use turbo navigator

--- tinder-ios/tinder-ios/SceneDelegate.swift
+++ tinder-ios/tinder-ios/SceneDelegate.swift
@@ -1,39 +1,21 @@
-import UIKit
 import Turbo
+import UIKit
+
+private let rootURL = URL(string: "http://localhost:3000")!
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    private lazy var navigationController = UINavigationController()
+    
+    let pathConfiguration = PathConfiguration(sources: [
+      .file(Bundle.main.url(forResource: "path-configuration", withExtension: "json")!)
+    ])
+
+    private lazy var navigator = TurboNavigator(pathConfiguration: pathConfiguration)
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
-        window!.rootViewController = navigationController
-        visit(url: URL(string: "https://turbo-native-demo.glitch.me")!)
-    }
-    
-    private func visit(url: URL) {
-        let viewController = VisitableViewController(url: url)
-        navigationController.pushViewController(viewController, animated: true)
-        session.visit(viewController)
-    }
-    
-    private lazy var session: Session = {
-        let session = Session()
-        session.delegate = self
-        return session
-    }()
-}
 
-extension SceneDelegate: SessionDelegate {
-    func session(_ session: Session, didProposeVisit proposal: VisitProposal) {
-        visit(url: proposal.url)
-    }
-    
-    func session(_ session: Session, didFailRequestForVisitable visitable: Visitable, error: Error) {
-        print("didFailRequestForVisitable: \(error)")
-    }
-    
-    func sessionWebViewProcessDidTerminate(_ session: Session) {
-        session.reload()
+        window!.rootViewController = navigator.rootViewController
+        navigator.route(rootURL)
     }
 }
