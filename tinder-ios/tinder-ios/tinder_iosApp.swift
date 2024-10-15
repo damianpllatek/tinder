bcb855cf1cd10e82fabdb522fcc765f491e3793c
Damian Płatek <damian.platek@infakt.pl>
2024-10-15 13:30:07 +0200

    Generate ios app

---
+++ tinder-ios/tinder-ios/tinder_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinder_iosApp.swift
+//  tinder-ios
+//
+//  Created by Damian Płatek on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct tinder_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tinder-ios/tinder-iosTests/tinder_iosTests.swift
@@ -0,0 +1,16 @@
+//
+//  tinder_iosTests.swift
+//  tinder-iosTests
+//
+//  Created by Damian Płatek on 15/10/2024.
+//
+
+import Testing
+
+struct tinder_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
